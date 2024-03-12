42d4bf7cc976c55f5b0d5d7ec90cf6c622327a08
Compil3 <vallardohaeris57@gmail.com>
2024-03-12 10:53:46 +0100

    added views and ejs

--- code/00-starting-project/app.js
+++ code/00-starting-project/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(port);
