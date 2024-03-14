8727f68df81336b4c0b51e9d92c504a5215c0de0
Compil3 <vallardohaeris57@gmail.com>
2024-03-14 13:16:23 +0100

    customers can update the quantity of their cart items, cart gets updated in front and backend

--- code/00-starting-project/controllers/cart.controller.js
+++ code/00-starting-project/controllers/cart.controller.js
@@ -22,8 +22,28 @@ async function addCartItem(req, res) {
     newTotalItems: cart.totalQuantity,
   });
 }
+async function updateCartItem(req, res) {
+  const cart = res.locals.cart;
+
+  const updatedItemData = cart.updateItem(
+    req.body.productId,
+    +req.body.quantity
+  );
+
+  req.session.cart = cart;
+
+  res.json({
+    message: 'Item updated!',
+    updatedCartData: {
+      newTotalQuantity: cart.totalQuantity,
+      newTotalPrice: cart.totalPrice,
+      updatedItemPrice: updatedItemData.updatedItemPrice,
+    },
+  });
+}
 
 module.exports = {
   addCartItem: addCartItem,
   getCart: getCart,
+  updateCartItem: updateCartItem,
 };
