e662fc742cf1a980710e3be110675f46ad6537a0
Compil3 <vallardohaeris57@gmail.com>
2024-03-14 20:54:54 +0100

    used MySQL instead of MongoDB. oders table - order_product table. combined data from these 2, users, addresses and products table to view all orders for a single customer

--- code/00-starting-project/models/order.model.js
+++ code/00-starting-project/models/order.model.js
@@ -1,4 +1,6 @@
 const db = require('../data/database');
+const Product = require('./product.model');
+const User = require('./user.model');
 
 class Order {
   // Status => pending, fulfilled, cancelled
@@ -18,10 +20,134 @@ class Order {
     this.id = orderId;
   }
 
+  static async transformOrderClass(order) {
+    // MySQL order Schema (What I receive):
+    // order.id
+    // order.userId
+    // order.date
+    // order.status
+    // order.total_quantity (total quantity of all the cart items)
+    // order.total_price (total price of all the cart items)
+
+    // userDATA SCHEMA:
+    // userdata = {
+    //   email,
+    //   password, // not needed
+    //   name,
+    //   address: {
+    //     street,
+    //     postalCode,
+    //     city,
+    //   },
+    // };
+    const user = await User.findById(order.userId);
+    const [address] = await db.execute(
+      `SELECT * FROM addresses WHERE id = (?) LIMIT 1`,
+      [user.addressId]
+    );
+
+    const userData = {
+      email: user.email,
+      name: user.fullname,
+      address: address[0], // MySQL only returns arrays => array[0]
+    };
+
+    // orderProduct schema:
+    // id
+    // orderId
+    // productId
+    // quantity (quantity of a product from the cart)
+    // single_price
+    // total_price (single_price * quantity)
+    const orderProductsQuery = `SELECT * FROM order_product WHERE orderId = (?)`;
+
+    const [orderProducts] = await db.execute(orderProductsQuery, [order.id]);
+    const cartItems = [];
+
+    for (const orderProduct of orderProducts) {
+      const [cartProduct] = await db.execute(
+        `SELECT * FROM products WHERE id = (?) LIMIT 1`,
+        [orderProduct.productId]
+      );
+      // convert cartProduct into object from class Product
+      const product = new Product(cartProduct[0]);
+
+      const cartItem = {
+        product,
+        quantity: orderProduct.quantity,
+        totalPrice: +orderProduct.total_price,
+      };
+
+      cartItems.push(cartItem);
+    }
+    // productDATA SCHEMA:
+    // const productData = {
+    //   items: [
+    //     {
+    //       product: {
+    //         title: '',
+    //         summary: '',
+    //         price: 0,
+    //         description: '',
+    //         image: '',
+    //         imagePath: '',
+    //         imageUrl: '',
+    //         id: '',
+    //       },
+    //       quantity: 0,
+    //       totalPrice: 2,
+    //     },
+    //     (product2 = {}),
+    //   ],
+    // };
+
+    const productData = {
+      items: cartItems,
+      totalQuantity: order.total_quantity,
+      totalPrice: +order.total_price,
+    };
+
+    return new Order(productData, userData, order.status, order.date, order.id);
+  }
+
+  static async transformOrders(orders) {
+    const transformedOrders = await Promise.all(
+      orders.map(this.transformOrderClass)
+    );
+    return transformedOrders;
+  }
+
+  static async findAll() {
+    const [orders] = await db.query(`SELECT * FROM orders ORDER BY date DESC`);
+    console.log('FIND ALL ORDERS: ', orders);
+    // make sure this returds orders as objects of class Order
+    return orders;
+  }
+
+  static async findAllForUser(userId) {
+    const [orders] = await db.execute(
+      `SELECT * FROM orders WHERE userId = (?) ORDER BY date DESC`,
+      [+userId]
+    );
+
+    return this.transformOrders(orders);
+  }
+
+  static async findById(orderId) {
+    const query = `SELECT * FROM orders WHERE id = (?) LIMIT 1`;
+    const [order] = await db.execute(query, [+orderId]);
+    console.log('FIND ORDER BY ID: ', order);
+    // make sure this returds orders as objects of class Order
+    return order[0];
+  }
+
   async save() {
     if (this.id) {
-      // Updating
+      // Update order - simplified: Only Admin can change Order status
+      const query = `UPDATE orders SET status = (?) WHERE id = (?)`;
+      await db.execute(query, [this.status, this.id]);
     } else {
+      // Create new Order
       const orderCart = this.productData;
       const orderCartItems = orderCart.items;
 
@@ -33,18 +159,14 @@ class Order {
         orderCart.totalPrice,
       ];
 
-      console.log('ORDER: ', order);
       const [orderResult] = await db.execute(
         'INSERT INTO orders (userId, date, status, total_quantity, total_price) VALUES (?, ?, ?, ?, ?)',
         order
       );
       const orderId = orderResult.insertId;
 
-      console.log('ORDER RESULT: ', orderResult);
-      console.log('orderCartItems: ', orderCartItems);
-
       for (const item of orderCartItems) {
-        // add each product from cart to the order_product table
+        // add each item from cart to the order_product table
         const orderProduct = [
           orderId,
           item.product.id,
@@ -57,8 +179,6 @@ class Order {
           'INSERT INTO order_product (orderId, productId, quantity, single_price, total_price) VALUES (?, ?, ?, ?, ?)',
           orderProduct
         );
-
-        console.log('orderProductResult: ', orderProductResult);
       }
     }
   }
