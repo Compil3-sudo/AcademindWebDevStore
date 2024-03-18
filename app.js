5c0043fa0ebc28473d5ec851c54f89f38f80204e
Compil3 <vallardohaeris57@gmail.com>
2024-03-18 10:56:46 +0100

    deployment should work now

--- app.js
+++ app.js
@@ -5,61 +5,58 @@ const csrf = require('csurf');
 require('dotenv').config();
 
 // Middlewares
-// const addCsrfTokenMiddleware = require('./middlewares/csrf');
-// const errorHandlerMiddleware = require('./middlewares/error-handler');
-// const checkAuthStatusMiddleware = require('./middlewares/check-auth');
-// const protectRoutesMiddleware = require('./middlewares/protect-routes');
-// const cartMiddleware = require('./middlewares/cart');
-// const updateCartPrices = require('./middlewares/update-cart-prices');
-// const notFoundMiddleware = require('./middlewares/not-found');
+const addCsrfTokenMiddleware = require('./middlewares/csrf');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+const checkAuthStatusMiddleware = require('./middlewares/check-auth');
+const protectRoutesMiddleware = require('./middlewares/protect-routes');
+const cartMiddleware = require('./middlewares/cart');
+const updateCartPrices = require('./middlewares/update-cart-prices');
+const notFoundMiddleware = require('./middlewares/not-found');
 
 // session
-// const expressSession = require('express-session');
-// const createSessionConfig = require('./config/session');
-// const sessionConfig = createSessionConfig();
+const expressSession = require('express-session');
+const createSessionConfig = require('./config/session');
+const sessionConfig = createSessionConfig();
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
-// app.use('/products/assets', express.static('product-data'));
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.use('/products/assets', express.static('product-data'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // routes
-// const authRoutes = require('./routes/auth.routes');
-// const productsRoutes = require('./routes/products.routes');
-// const baseRoutes = require('./routes/base.routes');
-// const adminRoutes = require('./routes/admin.routes');
-// const cartRoutes = require('./routes/cart.routes');
-// const ordersRoutes = require('./routes/orders.routes');
+const authRoutes = require('./routes/auth.routes');
+const productsRoutes = require('./routes/products.routes');
+const baseRoutes = require('./routes/base.routes');
+const adminRoutes = require('./routes/admin.routes');
+const cartRoutes = require('./routes/cart.routes');
+const ordersRoutes = require('./routes/orders.routes');
 
-// app.use(expressSession(sessionConfig));
-// app.use(csrf());
-// app.use(addCsrfTokenMiddleware); // this distributes generated tokens to all other middleware / route handlers and views
-// app.use(checkAuthStatusMiddleware);
-// app.use(cartMiddleware);
-// app.use(updateCartPrices);
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+app.use(addCsrfTokenMiddleware); // this distributes generated tokens to all other middleware / route handlers and views
+app.use(checkAuthStatusMiddleware);
+app.use(cartMiddleware);
+app.use(updateCartPrices);
 
-// app.use(baseRoutes);
-// app.use(authRoutes);
-// app.use(productsRoutes);
-// app.use('/cart', cartRoutes);
+app.use(baseRoutes);
+app.use(authRoutes);
+app.use(productsRoutes);
+app.use('/cart', cartRoutes);
 
-// app.use(protectRoutesMiddleware); // if this is here then notFoundMiddleware no longer executes
+app.use(protectRoutesMiddleware); // if this is here then notFoundMiddleware no longer executes
 // the following routes are protected by protectRoutesMiddleware
-// app.use('/admin', protectRoutesMiddleware, adminRoutes);
-// app.use('/orders', protectRoutesMiddleware, ordersRoutes);
+app.use('/admin', protectRoutesMiddleware, adminRoutes);
+app.use('/orders', protectRoutesMiddleware, ordersRoutes);
 
-// app.use(notFoundMiddleware);
+app.use(notFoundMiddleware);
 
-// app.use(errorHandlerMiddleware);
-
-// TESTING DEPLOYMENT
-app.get('/', (req, res) => res.send('Express on Vercel'));
+app.use(errorHandlerMiddleware);
 
 db.connect()
   .then((connection) => {
