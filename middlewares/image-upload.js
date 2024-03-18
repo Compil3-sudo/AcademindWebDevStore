c7b1fc32fe7432bf76c6f712f13b5212fbf690cd
Compil3 <vallardohaeris57@gmail.com>
2024-03-18 11:09:33 +0100

    fixing vercel deployment

--- middlewares/image-upload.js
+++ middlewares/image-upload.js
@@ -1,11 +1,14 @@
 const multer = require('multer');
-const uuid = require('uuid').v4;
+const { v4: uuidv4 } = require('uuid');
 
 const upload = multer({
   storage: multer.diskStorage({
-    destination: 'product-data/images',
-    filename: (req, file, callback) => {
-      callback(null, uuid() + '-' + file.originalname);
+    destination: function (req, file, cb) {
+      cb(null, '/tmp'); // Use the temporary directory provided by the serverless runtime
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      cb(null, `${uuidv4()}-${uniqueSuffix}-${file.originalname}`);
     },
   }),
 });
